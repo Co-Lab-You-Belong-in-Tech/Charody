083020bb20d9419bca51348fec7f359f5f7006b5
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-05-19 22:44:17 -0700

    +heroStyling
    styled hero w/ link:hover

--- client/src/components/HomePage/home.js
+++ client/src/components/HomePage/home.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
 import './home.css'
 export default function Home() {
     return(
         <div>
             <div className='hero'>
-                
+                <div>
+                    <p>Velit cillum proident ipsum elit ex in ex cupidatat quis ea reprehenderit aliqua. Consequat incididunt cillum dolor do velit ullamco incididunt aliqua ipsum. In elit ea anim minim consectetur ad ipsum in sint ullamco. Reprehenderit exercitation nulla id voluptate adipisicing aute anim dolore id quis mollit aliquip. Amet minim laboris aute et eu aliquip nostrud officia. Ut ullamco aute in labore consectetur quis cupidatat eu cupidatat et laborum consequat.</p>
+                    <Link className='link' to='/signUp'>Sign Up</Link>
+                </div>
             </div>
         </div>
     )
