9c2c163f9fcca0281de30ba384e1d58a2d31fbe5
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-05-23 01:48:29 -0700

    sign up for official

---
+++ client/src/components/SignUp/Official/official.js
@@ -0,0 +1,9 @@
+export default function Official(){
+    return(
+        <form>
+            <input placeholder='Email'></input>
+            <input placeholder='Password'></input>
+            <button type='submit' className='buttonColored full'>Create an Account</button>
+        </form>
+    )
+}
