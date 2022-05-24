9e847a2de3cb63a608cb804683aabb61ee5375ff
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-05-23 21:11:30 -0700

    addde in logo

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom"
 
+import Logo from '../../static/images/Logo.png'
 import './navbar.css'
 import '../components.css'
 export default function Navbar() {
     return(
         <div className='navbar'>
             <div>
-                <NavLink className='navLink' to='/'>Charody</NavLink>
+                <NavLink className='navLink' to='/'><img src={Logo}></img></NavLink>
             </div>
             <div>
                 <NavLink className='navLink' to='/aboutUs'>About Us</NavLink>
