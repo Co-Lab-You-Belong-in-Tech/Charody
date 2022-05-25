e02032cba20c62513e4e6ecb98b782ff079d5bc3
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-05-25 14:33:37 -0700

    verify.js
    combines previous forms and onclick data submission button

--- client/src/components/ProfileCreation/Verify/verify.js
+++ client/src/components/ProfileCreation/Verify/verify.js
@@ -1,10 +1,18 @@
 import { useSelector } from 'react-redux'
+import './verify.css'
+
+import Info from '../Info/info'
+import Home from '../Home/home'
 
 export default function Verify() {
     const data = useSelector(state => state.signup.value)
     return(
-        <div>
-            {JSON.stringify(data)}
+        <div className='verifyForm'>
+            <Info/>
+            <Home/>
+            <button onClick={e => {
+                // sends const data to database
+            }}>Done</button>
         </div>
     )
 }
