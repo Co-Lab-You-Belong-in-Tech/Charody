128264693a34ad6e37895fe7caee7df4d2636795
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-05-29 23:31:20 -0700

    conditionalRendering navbar

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom"
+import { useAuth } from "../../context/AuthContext"
 
 import Logo from '../../static/images/Logo.png'
 import './navbar.css'
 import '../components.css'
+import { Fragment } from "react"
 export default function Navbar() {
+    const auth = useAuth()
     return(
         <div className='navbar'>
             <div>
@@ -11,8 +14,30 @@ export default function Navbar() {
             </div>
             <div>
                 <NavLink className='navLink' to='/aboutUs'>About Us</NavLink>
-                <NavLink className='navLink buttonColored invert' to='/login'>Login</NavLink>
-                <NavLink className='navLink buttonColored' to='/signUp/homeowner'>Sign Up</NavLink>
+                {/* homeownerNav */}
+                {auth.user.type == 'homeowner' ? (
+                    <Fragment>
+                        <NavLink className='navLink' to='/editProfile'>Profile</NavLink>
+                    </Fragment>
+                ) : ''}
+                {/* officialNav */}
+                {auth.user.type == 'official' ? (
+                    <Fragment>
+                        <NavLink className='navLink' to='/search'>Search</NavLink>
+                    </Fragment>
+                ) : ''}
+                {/* login/logout */}
+                {auth.user ? (
+                    <Fragment>
+                        <NavLink className='navLink buttonColored invert' to='/login'>Login</NavLink>
+                        <NavLink className='navLink buttonColored' to='/signUp/homeowner'>Sign Up</NavLink>
+                    </Fragment>
+                ) : (
+                    <Fragment>
+                        <button className='buttonText' onClick={e => auth.signout()}>Logout</button>
+                    </Fragment>
+                ) }
+                
             </div>
         </div>
     )
