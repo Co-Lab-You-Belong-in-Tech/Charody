43e5d074947f0f6fd8e2c96091c52cd51eed87fe
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-05-31 23:24:42 -0700

    forgotPassword submit-result

--- client/src/components/ForgotPassword/forgotPassword.js
+++ client/src/components/ForgotPassword/forgotPassword.js
@@ -6,24 +6,28 @@ import { useState } from "react";
 
 export default function ForgotPassword(){
     const [email, setEmail] = useState()
+    const [pageContent, setPageContent] = useState(
+        <form onSubmit={e => {
+            // does something to signify this user forgot their email
+            setPageContent(<p>Your password reset has been received. Someone will email you with instructions within <b>2-3 business days</b>.</p>)
+        }}>
+            <p>Please enter the email address associated with your account for reset information. </p>
+            <div>
+                <label>Email Address</label>
+                <input placeholder='Email' onChange={e => {
+                    setEmail(e.target.value)
+                }}></input>
+            </div>
+            <button className='buttonColored full'>Request Reset</button>
+        </form>
+    )
     return(
         <ImgTextBlock
             txt={() => {
                 return(
                     <div className='forgotPasswordForm'>
                         <h2>Forgot Your Password?</h2>
-                        <p>Please enter the email address associated with your account for reset information. </p>
-                        <form onSubmit={e => {
-                            // sends data
-                        }}>
-                            <div>
-                                <label>Email Address</label>
-                                <input placeholder='Email' onChange={e => {
-                                    setEmail(e.target.value)
-                                }}></input>
-                            </div>
-                            <button className='buttonColored full'>Request Reset</button>
-                        </form>
+                        {pageContent}
                     </div>
                 )
             }}
