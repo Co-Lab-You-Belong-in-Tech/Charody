f11742d895e90b05293b019579d44080e0ede296
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-06-01 20:51:38 -0700

    signup restyle

--- client/src/components/SignUp/signUp.js
+++ client/src/components/SignUp/signUp.js
@@ -34,7 +34,6 @@ export default function SignUp(){
                 </div>
                 
             </div>
-            
             <div>
                 <div className='navLink'>
                     <NavLink className='navLink' activeStyle={activeStyle} to={`/signUp/homeowner`}>Homeowner</NavLink>
